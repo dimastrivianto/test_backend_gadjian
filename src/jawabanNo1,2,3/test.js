53844d1e592e4280dfb63ae7effff9df61460e23
dimtriv <dimas.trivianto@gmail.com>
2020-09-18 17:10:09 +0700

    finish

--- src/jawabanNo1,2,3/test.js
+++ src/jawabanNo1,2,3/test.js
@@ -2,14 +2,15 @@
 let character = "abcde"
 
 const reverse = (character) => {
-  let charLength = findCharLength(character)
-  let reverseString = ""
   function findCharLength(character){
     let length = 0
     while(character[length] !== undefined)
-      length++;
+    length++;
     return length;
   }
+  
+  let charLength = findCharLength(character)
+  let reverseString = ""
   for(let i = charLength-1; i>=0; i--){
     reverseString += character[i]
   }
@@ -17,15 +18,16 @@ const reverse = (character) => {
 }  
   reverse(character)
 
+// namanya ganti array
 // jawaban no 2
 const fibonacci=(n)=>{
-  let stringFibonacci = []
+  let arrayFibonacci = []
   for(let i = 0; i<=n; i++){
-    if(i === 0) stringFibonacci.push(i)
-    else if(i === 1) stringFibonacci.push(i)
-    else stringFibonacci.push(stringFibonacci[i-1]+stringFibonacci[i-2])
+    if(i === 0) arrayFibonacci.push(i)
+    else if(i === 1) arrayFibonacci.push(i)
+    else arrayFibonacci.push(arrayFibonacci[i-1]+arrayFibonacci[i-2])
   }
-  return stringFibonacci.join(" ")
+  return arrayFibonacci.join(" ")
 }
 
 fibonacci(9)
@@ -33,17 +35,28 @@ fibonacci(9)
 // jawaban no 3
 function combination(n,r){
   if(n<=r) {
-    return "sorry, n has to be bigger than r, let's try again ;)"
+    return "sorry, n has to be bigger than r, please try again"
   }else{
     let topResult = n
     for(let i = 1; i<n; i++){
       topResult *= n-i
     }
-    
+    let firstBottomResult = r
+    for(let i = 1; i<r; i++){
+      firstBottomResult *= r-i
+    }
+    let secondBottomResult = n-r
+    for(let i = 1; i<(n-r); i++){
+      secondBottomResult *= (n-r)-i
+    }
+
+    let finalResult = topResult / (firstBottomResult * secondBottomResult)
+    return finalResult
   }
 }
 
-combination(4,3)
+combination(4,2)
 
 module.exports.fibonacci = fibonacci
 module.exports.reverse = reverse
+
--- src/jawabanNo4dan5/jawabanNo4dan5.js
+++ src/jawabanNo4dan5/jawabanNo4dan5.js
@@ -2,6 +2,11 @@ const conn = require('./database')
 const router = require('express').Router()
 const formula = require('../jawabanNo1,2,3/test')
 
+
+//////////////////
+///Jawaban no 4///
+/////////////////
+
 /////////
 ///GET///
 /////////
@@ -47,21 +52,6 @@ router.post('/api/employees', (req, res) => {
   })
 })
 
-router.post('/api/reverse',  (req, res) => {
-  
-  let {character} = req.body
-  let result = formula.reverse(character)
-
-      res.status(200).send({result})
-})
-
-router.post('/api/fibonacci', (req, res) => {
-  const {n} = req.body
-  let result = formula.fibonacci(n)
-
-  res.status(200).send(result)
-})
-
 //////////
 ///PUT///
 //////////
@@ -97,5 +87,70 @@ router.delete('/api/employees/:id', (req, res) => {
 
     res.status(204).send()
   })
+})
+
+
+
+
+//////////////////
+// Jawaban no 5//
+////////////////
+
+router.post('/api/reverse',  (req, res) => {
+  
+  const {character} = req.body
+  let result = formula.reverse(character)
+
+      res.status(200).send({result})
+})
+
+router.post('/api/fibonacci', (req, res) => {
+  const {n} = req.body
+  let result = formula.fibonacci(n)
+
+  res.status(200).send(result)
+})
+
+// combination with repetition
+router.post('/api/combination', (req, res) =>{
+  let result = []
+  const arr = []
+  const {n,r} = req.body
+
+  for(let i = 1; i<=n; i++){
+    arr.push(i)
+  }
+
+  function disp(x) {
+    // console.log(x)
+    result = [...result, x.join(" ")]
+    return ;
+  }
+
+  function pick(r, got, start, arr) {
+    if (got.length == r) {
+        disp(got);
+        return 1;
+        
+    }
+    for (var i = start; i < arr.length; i++) {
+        got.push(arr[i]);
+        pick(r, got, i, arr);
+        got.pop();
+    }
+  }
+  pick(r,[],0, arr)
+
+  // randomize
+  let length = result.length
+  let finalResult = []
+  for(let i = 0; i<=length-1; i++){
+    let random = Math.floor(Math.random()*result.length)
+    
+    finalResult[i] = result[random]
+    result.splice(random, 1)
+  }
+  
+  res.status(200).send(finalResult)
 })
 module.exports = router
